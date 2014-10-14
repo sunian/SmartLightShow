717a84528fe1da26707a09b53e10d58b652eed86
Arjun Shankar <sarjun93@gmail.com>
2014-10-14 01:17:49 -0400

    Added initial attempt at fft to light show scheme logic

---
+++ SmartLightShow/SoundProcessing/FFTProcessor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Dsp;
+
+namespace SmartLightShow.SoundProcessing
+{
+	class FFTProcessor
+	{
+		private int minFreq;
+		private int maxFreq;
+		private int sampleRate;
+		private int numBuckets;
+
+		static const double MIN_MAGNITUDE = 0.003;
+
+		public FFTProcessor(int min, int max, int sampleRate, int lightStreams)
+		{
+			minFreq = min;
+			maxFreq = max;
+			this.sampleRate = sampleRate;
+			numBuckets = lightStreams;
+		}
+
+		public bool[] ProcessFFT(Complex[] fft)
+		{
+			int fftLength = fft.Length;
+			bool[] lights = new bool[numBuckets];
+			for (int i = 0; i < fftLength; i++)
+			{
+				double freq = ((double) i) / fftLength * sampleRate;
+				double mag = Math.Sqrt(fft[i].X * fft[i].X + fft[i].Y * fft[i].Y);
+				double phase = Math.Tan(fft[i].Y / fft[i].X);
+
+				if (mag > MIN_MAGNITUDE)
+				{
+					minFreq = (int) Math.Min(minFreq, freq);
+					maxFreq = (int) Math.Max(maxFreq, freq);
+
+					double bucketSep = (maxFreq - minFreq) / (double)numBuckets;
+					int bucket = 1;
+					while (freq < bucket * bucketSep + minFreq)
+					{
+						bucket++;
+					}
+					bucket--;
+					lights[bucket] = true;
+				}
+			}
+
+			return lights;
+		}
+	}
+}
