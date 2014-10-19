e4beb897b35ee2dc0e03901ca44d916ebbc86ca9
Arjun Shankar <sarjun93@gmail.com>
2014-10-19 00:09:09 -0400

    Worked on wav file analysis. SHould work except for the fact that some things in wav header are in little endian... randomly...

--- SmartLightShow/SoundProcessing/Analyzers/FileAnalyzer.cs
+++ SmartLightShow/SoundProcessing/Analyzers/FileAnalyzer.cs
@@ -1,5 +1,6 @@
 ﻿using NAudio.Wave;
 using System;
+using System.IO;
 
 namespace SmartLightShow.SoundProcessing.Analyzers {
     class FileAnalyzer : Analyzer {
@@ -13,15 +14,39 @@ namespace SmartLightShow.SoundProcessing.Analyzers {
 
         override public void RunAnalysis() {
             Console.WriteLine("You started file analysis");
-            int bytesRecorded = 0;
-            int bufferIncrement = 1024;
-            byte[] buffer = new byte[bufferIncrement];
+			BinaryReader reader = new BinaryReader(fileReader);
 
-            do {
-                bytesRecorded = fileReader.Read(buffer, 0, bufferIncrement);
-                float sample32 = BitConverter.ToSingle(buffer, 0);
-                sampleAggregator.Add(sample32);
-            } while (bytesRecorded != 0);
+			int chunkID = reader.ReadInt32();
+			int fileSize = reader.ReadInt32();
+			int riffType = reader.ReadInt32();
+			int fmtID = reader.ReadInt32();
+			int fmtSize = reader.ReadInt32();
+			int fmtCode = reader.ReadInt16();
+			int channels = reader.ReadInt16();
+			int sampleRate = reader.ReadInt32();
+			int fmtAvgBPS = reader.ReadInt32();
+			int fmtBlockAlign = reader.ReadInt16();
+			int bitDepth = reader.ReadInt16();
+
+			if (fmtSize == 18)
+			{
+				// Read any extra values
+				int fmtExtraSize = reader.ReadInt16();
+				reader.ReadBytes(fmtExtraSize);
+			}
+
+			int dataID = reader.ReadInt32();
+			int dataSize = reader.ReadInt32();
+			byte[] data = reader.ReadBytes(dataSize);
+
+			int bytesPerSecond = bitDepth * sampleRate * channels / 8;
+
+			Console.WriteLine(fmtBlockAlign * bitDepth + " " + dataSize + " " + data.Length);
+			for (int i = 0; i < dataSize; i+=fmtBlockAlign)
+			{
+				float sample32 = BitConverter.ToSingle(data, i);
+				sampleAggregator.Add(sample32);
+			}
 
             // Hold for now, remove this later obviously.
             while (true) {}
